3eb8f8de211b359310199abff731346f57e593ae
AbdulRehman <111703045+Abdul641@users.noreply.github.com>
2024-02-04 15:13:57 -0500

    Feb 4

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const projectMainDiv = document.querySelector(".allProject");
+const secondProject = document.querySelector(".secondProject");
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+//var x = document.getElementById("demo");
+console.log("Browser width: " + w + ", height: " + h + ".");
+//x.innerHTML = "Browser width: " + w + ", height: " + h + ".";
+
+function checkingWidth() {
+  if (window.innerWidth > 1000) {
+    let secondProjectImage = document.createElement("img");
+    secondProjectImage.src = "./pictures/fourth.jpg";
+    secondProjectImage.append(secondProjectImage);
+  }
+}
+
+checkingWidth();
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
